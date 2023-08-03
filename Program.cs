43a3cc49fe561774f497bf370a94d4bf375667fd
nyakiomwangi <cnyakio@usiu.ac.ke>
2023-08-03 16:19:54 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        //C# program to print all negative elements in array
+        /* Input array size */
+        Console.Write("Enter size of array: ");
+        int size = Convert.ToInt32(Console.ReadLine());
+        int[] arr = new int[size];
+        /* Input elements in array */
+        Console.WriteLine("Enter {0} elements in the array", size);
+        for (int i = 0; i < size; i++)
+        {
+            arr[i] = Convert.ToInt32(Console.ReadLine());
+        }
+        /*Print all negative elements of array*/
+        Console.Write("\nAll negative elements in array are : ");
+        for (int i = 0; i < size; i++)
+        {
+            /* If current array element is negative */
+            if (arr[i] < 0)
+            {
+                Console.Write("{0}\t", arr[i]);
+            }
+        }
+        Console.ReadLine();
+    }
+}
